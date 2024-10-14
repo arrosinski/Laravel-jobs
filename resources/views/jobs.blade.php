a4fa21de56c8e9296385a660dc785292c4eb820b
arkadiuszrosinski <ar.rosinski@gmail.com>
2024-10-14 16:05:56 +0200

    main: add pagination and more
    reliable getting data

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -6,16 +6,20 @@
     <x-slot name="title">
         Job Listings
     </x-slot>
-    <ul>
-    @foreach ($jobs as $job)
-          <li>
-              <a  href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200">
-        {{ $job['title'] }}
-        Description {{ $job['description'] }}
-                  <br>
-          Pays : {{ $job['salary'] }}
-              </a>
-          </li>
-    @endforeach
-    </ul>
-</x-layout >
+    <div class="space-y-4">
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500"> {{ $job->employer->name}}</div>
+                <div>
+                    <strong>{{ $job['title'] }}</strong>
+                    Description: {{ $job['description'] }}
+                    <br>
+                    Pays: {{ $job['salary'] }}
+                </div>
+            </a>
+        @endforeach
+        <div>
+            {{$jobs->links()}}
+        </div>
+    </div>
+</x-layout>
