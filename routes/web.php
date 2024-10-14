4f423dcc4aa0cb72e79c3d478a47a4e9e8f3fc7d
arkadiuszrosinski <ar.rosinski@gmail.com>
2024-10-14 17:44:38 +0200

    main: create.blade.php - add
    create method with csrf

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Models\Job;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('home');
 });
@@ -9,25 +10,42 @@ Route::get('/', function () {
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->simplePaginate(3);
 
-    return view('jobs',
+    return view('jobs.index',
         [
-            'jobs' => $jobs
+            'jobs' => $jobs,
         ]);
+});
+
+Route::post('/jobs', function () {
+    $job = new Job;
+
+    $job->title = request('title');
+    $job->description = request('description');
+    $job->salary = request('salary');
+    $job->employer_id = 1;
+    $job->save();
+
+    /*Job::create([
+        'title' => request('title'),
+        'description' => request('description'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);*/
+
+    return redirect('/jobs');
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
-
 
 Route::get('/contact', function () {
     return view('contact');
 });
-
-
-
-
-
