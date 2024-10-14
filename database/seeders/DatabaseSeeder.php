fcc363f2873534623b5c56f1117d364377ffbada
arkadiuszrosinski <ar.rosinski@gmail.com>
2024-10-14 16:14:04 +0200

    main: DatabaseSeeder.php - add
    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employer;
+use App\Models\Job;
+use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+         User::factory(10)->create();
+         Job::factory(20)->create();
+         Employer::factory(10)->create();
+         //Tag::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'first_name' => 'John',
+//            'last_name' => 'Doe',
+//            'email' => 'test@example.com',
+//        ]);
     }
 }
