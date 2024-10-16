16b396c8b23cd52b1aeea223aa7d6afb3bcab9e6
arkadiuszrosinski <ar.rosinski@gmail.com>
2024-10-16 12:22:01 +0200

    main: web.php - add JobController
    and fix routes

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-//index
 Route::get('/jobs/', [JobController::class, 'index']);
-//store
 Route::post('/jobs', function () {});
-//create
 Route::get('/jobs/create', [JobController::class, 'create']);
-//edit
 Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-//update
 Route::patch('/jobs/{id}/edit', [JobController::class, 'update']);
-//destroy
 Route::delete('/jobs/{id}/edit', [JobController::class, 'destroy']);
-//show
 Route::get('/jobs/{job}', [JobController::class, 'show']);
-
 Route::get('/contact', function () {
     return view('contact');
 });
