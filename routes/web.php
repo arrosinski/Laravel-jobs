430b798e6c2ff2267e002dbf3a2217d090304b18
arkadiuszrosinski <ar.rosinski@gmail.com>
2024-10-16 12:50:30 +0200

    main: web.php - add comments

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,17 @@ Route::get('/', function () {
 });
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
+//grouped controller routes
 Route::controller(JobController::class)->group(function () {
     Route::get('/jobs/', 'index');
     Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
     Route::get('/jobs/create', 'create');
+    Route::post('/jobs', 'store');
     Route::get('/jobs/{job}/edit', 'edit');
     Route::patch('/jobs/{id}/edit', 'update');
     Route::delete('/jobs/{id}/edit', 'destroy');
 });
+
+//you can use resource when you have index,show,create,store,edit,update,destroy methods in your controller
 //Route::resource('jobs', JobController::class);
 
